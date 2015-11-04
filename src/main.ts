f7e7e255529f01546a0ec880d0f5ed3a22ec8e26
Josh Heyse <josh@heyse.us>
2015-11-03 23:10:38 -0600

    es6 classes, with typesaftey and compile time visibility

--- src/main.ts
+++ src/main.ts
@@ -1,63 +1,70 @@
 /// <reference path="../typings/jquery/jquery.d.ts" />
 
-var AdditionCalculator = function(operator) {
-    this.operator = operator;
-};
-
-AdditionCalculator.prototype.performOperation = function(a: number, b: number) {
-  if(this.operator === 'Add') {
-    return a + b;
+class AdditionCalculator {
+  constructor(public operator: string) {
   }
-  else if(this.operator === 'Subtract') {
-    return a - b;
+
+  performOperation(a: number, b: number) {
+    if(this.operator === 'Add') {
+      return a + b;
+    }
+    else if(this.operator === 'Subtract') {
+      return a - b;
+    }
   }
-};
+}
 
-
-var ProductCalculator = function(operator) {
-    this.operator = operator;
-};
-
-ProductCalculator.prototype.perfromOperation = function(a: number, b: number) {
-  if(this.operator === 'Multiply') {
-    return a * b;
+class ProductCalculator {
+  constructor(public operator: string) {
   }
-  else if(this.operator === 'Divide') {
-    return a / b;
-  }
-};
 
-var Controller = function() {
-  this.calculators = [
-    new AdditionCalculator('Add'),
-    new AdditionCalculator('Subtract'),
-    new ProductCalculator('Multiply'),
-    new ProductCalculator('Divide')
-    ];
-};
-
-Controller.prototype.attach = function(e) {
-  this.result = $('#result');
-  this.operations = $('#operations');
-  var self = this;
-  this.calculators.forEach(function(calculator) {
-    $('<button class="btn btn-default">' + calculator.operator + '</button>')
-      .click(function() { self.performOperation(calculator); })
-      .appendTo(self.operations);
-  });
-};
-
-Controller.prototype.performOperation = function(calculator) {
-  this.result.val('');
-  var a = $('#valueA').val();
-  var b = $('#valueB').val();
-  try {
-    this.result.val('The result of ' +  a + ' and ' + b + ' is ' + calculator.performOperation(a, b));
+  perfromOperation(a: number, b: number) {
+    if(this.operator === 'Multiply') {
+      return a * b;
+    }
+    else if(this.operator === 'Divide') {
+      return a / b;
+    }
   }
-  catch(err) {
-    this.result.val('There was an error performing the operation');
+}
+
+class Controller {
+  private calculators : any[];
+  private result: JQuery;
+  private operations: JQuery;
+
+  constructor() {
+    this.calculators = [
+      new AdditionCalculator('Add'),
+      new AdditionCalculator('Subtract'),
+      new ProductCalculator('Multiply'),
+      new ProductCalculator('Divide')
+      ];
   }
-};
+
+  attach() {
+    this.result = $('#result');
+    this.operations = $('#operations');
+    var self = this;
+    this.calculators.forEach(function(calculator) {
+      $('<button class="btn btn-default">' + calculator.operator + '</button>')
+        .click(function() { self.performOperation(calculator); })
+        .appendTo(self.operations);
+    });
+  }
+
+  performOperation(calculator) {
+    this.result.val('');
+    var a = $('#valueA').val();
+    var b = $('#valueB').val();
+    try {
+      this.result.val('The result of ' +  a + ' and ' + b + ' is ' + calculator.performOperation(a, b));
+    }
+    catch(err) {
+      this.result.val('There was an error performing the operation');
+    }
+  }
+}
 
 $(function(){
   var controller = new Controller();
